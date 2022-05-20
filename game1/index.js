af00f788cab9e82b13067d3bc6f45543f3e29a3d
Rafael Gamarra <rafaelgamas@gmail.com>
2022-05-20 17:39:51 -0400

    upload Game 1

---
+++ game1/index.js
@@ -0,0 +1,236 @@
+"use strict";
+function init() {
+  const canvas = document.querySelector("canvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  const scoreEl = document.querySelector("#scoreEl");
+  const startGameBtn = document.querySelector("#startGameBtn");
+  const modeEl = document.querySelector("#modeEl");
+  const totalScore = document.querySelector("#totalScore");
+  class Player {
+    constructor(x, y, radius, color) {
+      this.x = x;
+      this.y = y;
+      this.radius = radius;
+      this.color = color;
+    }
+    draw() {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      ctx.fillStyle = this.color;
+      ctx.fill();
+    }
+  }
+
+  class Projectile {
+    constructor(x, y, radius, color, velocity) {
+      this.x = x;
+      this.y = y;
+      this.radius = radius;
+      this.color = color;
+      this.velocity = velocity;
+    }
+    draw() {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      ctx.fillStyle = this.color;
+      ctx.fill();
+    }
+    update() {
+      this.draw();
+      this.x = this.x + this.velocity.x;
+      this.y = this.y + this.velocity.y;
+    }
+  }
+  class Enemy {
+    constructor(x, y, radius, color, velocity) {
+      this.x = x;
+      this.y = y;
+      this.radius = radius;
+      this.color = color;
+      this.velocity = velocity;
+    }
+    draw() {
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      ctx.fillStyle = this.color;
+      ctx.fill();
+    }
+    update() {
+      this.draw();
+      this.x = this.x + this.velocity.x;
+      this.y = this.y + this.velocity.y;
+    }
+  }
+  const friction = 0.99;
+  class Particle {
+    constructor(x, y, radius, color, velocity) {
+      this.x = x;
+      this.y = y;
+      this.radius = radius;
+      this.color = color;
+      this.velocity = velocity;
+      this.alpha = 1;
+    }
+    draw() {
+      ctx.save();
+      ctx.globalAlpha = 0.1;
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+      ctx.fillStyle = this.color;
+      ctx.fill();
+      ctx.restore();
+    }
+    update() {
+      this.draw();
+      this.velocity.x *= friction;
+      this.velocity.y *= friction;
+      this.x = this.x + this.velocity.x;
+      this.y = this.y + this.velocity.y;
+      this.alpha -= 0.01;
+    }
+  }
+  let x = canvas.width / 2;
+  let y = canvas.height / 2;
+  let player = new Player(x, y, 10, "white");
+  let projectiles = [];
+  let enemies = [];
+  let particles = [];
+  let score = 0;
+
+  function reset() {
+    player = new Player(x, y, 10, "white");
+    projectiles = [];
+    enemies = [];
+    particles = [];
+    score = 0;
+    scoreEl.innerHTML = score;
+    totalScore.innerHTML = score;
+  }
+
+  function spawnEnemies() {
+    setInterval(() => {
+      const radius = Math.random() * (30 - 4) + 4;
+      let x;
+      let y;
+      if (Math.random() < 0.5) {
+        x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+        y = Math.random() * canvas.height;
+      } else {
+        x = Math.random() * canvas.width;
+        y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+      }
+      const color = `hsl(${Math.random() * 360},50%,50%)`;
+      const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+      const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle),
+      };
+      enemies.push(new Enemy(x, y, radius, color, velocity));
+    }, 1000);
+  }
+  //Main Animation
+  let animationId;
+  score = 0;
+  function animate() {
+    animationId = requestAnimationFrame(animate);
+    ctx.fillStyle = "rgba(0,0,0,0.1)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    player.draw();
+
+    particles.forEach((particle, partIndex) => {
+      particle.update();
+      if (particle.alpha <= 0) {
+        particles.splice(partIndex, 1);
+      } else {
+        particle.update();
+      }
+    });
+    //remove projectiles from the edge of the game
+    projectiles.forEach((projectile, Pindex) => {
+      projectile.update();
+      if (
+        projectile.x + projectile.radius < 0 ||
+        projectile.x - projectile.radius > canvas.width ||
+        projectile.y + projectile.radius < 0 ||
+        projectile.y - projectile.radius > canvas.height
+      ) {
+        setTimeout(() => {
+          projectiles.splice(Pindex, 1);
+        }, 0);
+      }
+    });
+    enemies.forEach((enemy, Eindex) => {
+      enemy.update();
+      const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
+      if (dist - enemy.radius - player.radius < 1) {
+        cancelAnimationFrame(animationId);
+        modeEl.style.display = "flex";
+        totalScore.innerHTML = score;
+      }
+      projectiles.forEach((projectile, Pindex) => {
+        const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
+        //enemy hit
+        if (dist - enemy.radius - projectile.radius < 1) {
+          //creating a exploxions
+          for (let i = 0; i < enemy.radius * 2; i++) {
+            particles.push(
+              new Particle(
+                projectile.x,
+                projectile.y,
+                Math.random() * 3,
+                enemy.color,
+                {
+                  x: (Math.random() - 0.5) * (Math.random() * 6),
+                  y: (Math.random() - 0.5) * (Math.random() * 6),
+                }
+              )
+            );
+          }
+          if (enemy.radius - 10 > 5) {
+            //Increase score points
+            score += 100;
+            scoreEl.innerHTML = score;
+            gsap.to(enemy, {
+              radius: (enemy.radius -= 5),
+            });
+
+            setTimeout(() => {
+              projectiles.splice(Pindex, 1);
+            }, 0);
+          } else {
+            //bonus by removing from the scene an object
+            score += 150;
+            scoreEl.innerHTML = score;
+            setTimeout(() => {
+              enemies.splice(Eindex, 1);
+              projectiles.splice(Pindex, 1);
+            }, 0);
+          }
+        }
+      });
+    });
+  }
+
+  //Click Event
+  startGameBtn.addEventListener("click", () => {
+    reset();
+    animate();
+    spawnEnemies();
+    modeEl.style.display = "none";
+  });
+
+  window.addEventListener("click", (event) => {
+    const angle = Math.atan2(
+      event.clientY - canvas.width / 2,
+      event.clientX - canvas.width / 2
+    );
+    const velocity = {
+      x: Math.cos(angle) * 5,
+      y: Math.sin(angle) * 5,
+    };
+    projectiles.push(new Projectile(x, y, 5, "white", velocity));
+  });
+  //call the functions
+}
